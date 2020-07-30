57a2c5976617623c4fada9b78676fba5509291b3
PrithirajN <prithirajnandan@gamil.com>
2020-07-30 09:29:52 +0530

    Created Burger Container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Layout from './components/Layout/Layout'
+import Layout from './components/Layout/Layout';
+import Burger from './containers/Burger/Burger';
 
 
 class App extends Component {
@@ -7,9 +8,8 @@ class App extends Component {
         return (
             <div>
                 <Layout>
-                    <p>Test</p>
+                    <Burger/>
                 </Layout>
-
             </div>
         );
     }
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './Layout.module.css'
 import Aux from '../../hoc/Auxiliary'
 
 const layout = (props) => {
     return (
         <Aux>
             <div>Toolbar, Sidebar, Backdrop</div>
-            <main>
+            <main className={styles.Content}>
                 {props.children}
             </main>
         </Aux>
