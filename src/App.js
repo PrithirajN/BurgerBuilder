464108010d38d734f7775b5a4ee39033710978b9
Prithiraj Nandan <prithirajnandan@gmail.com>
2020-07-30 09:35:39 +0530

    Renaming Burger container to BurgerMaker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
-import Burger from './containers/Burger/Burger';
+import Burger from './containers/BurgerMaker/BurgerMaker';
 
 
 class App extends Component {
