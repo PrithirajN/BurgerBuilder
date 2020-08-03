0a8b213ba2a1d2bf25a254a1956e835062143809
Prithiraj Nandan <prithirajnandan@gmail.com>
2020-08-03 09:31:24 +0530

    Added order button

--- src/containers/BurgerMaker/BurgerMaker.js
+++ src/containers/BurgerMaker/BurgerMaker.js
@@ -19,7 +19,17 @@ class BurgerMaker extends Component
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePuschaseState(ingredients) {
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey]
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+        this.setState({ purchasable: sum > 0 });
     }
 
     addIngredientHandler = (type) => {
@@ -36,6 +46,7 @@ class BurgerMaker extends Component
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+        this.updatePuschaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -55,6 +66,7 @@ class BurgerMaker extends Component
             totalPrice: newPrice,
             ingredients: updatedIngredients
         })
+        this.updatePuschaseState(updatedIngredients)
     }
 
     render() {
@@ -70,7 +82,9 @@ class BurgerMaker extends Component
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}/>
+                    disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
+                    price={this.state.totalPrice}/>
             </Aux>
         );
     }
